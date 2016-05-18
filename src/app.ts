591b5b960d924524d06c6ff3ef0b33e46a5b72aa
Andrew Watson <awatson@gopro.com>
2016-05-18 17:04:56 -0500

    More updates to infrastructure to hopefully make the site work.

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ import index from './routes/index';
 const app: express.Express = express();
 
 //view engine setup
-app.set('views',path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'jade');
 
 //uncomment after placing your favicon in /public

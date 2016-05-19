c10121cb48070b6a5db84e84216fb1c1eb31c99f
Andrew Watson <awatson@gopro.com>
2016-05-19 14:41:14 -0500

    Further cleanup of main site "class"

--- src/site.ts
+++ src/site.ts
@@ -1,34 +1,19 @@
 'use strict';
 
-/**
- * Module dependencies
- */
 import app from './app';
 import * as debugModule from 'debug';
 import * as http from 'http';
 
 const debug = debugModule('express:server');
-
 const port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-const serverIpAddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 var server = http.createServer(app);
-
-/**
- * Listen on provided port,on all network interfaces.
- */
-server.listen(port, serverIpAddress);
+server.listen(port, process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 server.on('error', onError);
 server.on('listening', onListening);
 
-/**
- * Event listener for HTTP server "error" event.
- */
 function onError(error) {
   if (error.syscall != 'listen') {
     throw error;
@@ -36,7 +21,6 @@ function onError(error) {
   
   var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
   
-  //handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
       console.error(bind + 'requires elevated privileges');
@@ -50,10 +34,7 @@ function onError(error) {
       throw error;
   }
 }
-  
-/**
- * Event listener for HTTP server "listening" event.
- */
+
 function onListening() {
   var addr = server.address();
   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;

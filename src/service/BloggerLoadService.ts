3e60d187ce31e3476f61199c30fb253023098d7b
Andrew Watson <awatson@gopro.com>
2016-05-24 16:14:38 -0500

    Figure out correct way to import nodejs libs.

--- src/service/BloggerLoadService.ts
+++ src/service/BloggerLoadService.ts
@@ -3,15 +3,18 @@
 var env = require('node-env-file');
 env('.env');
 
-import * as Q from 'q'; 
+import * as https from 'https';
+import * as Q from 'q';
 
 export class BloggerLoadService {
+        
     /**
      * 
      */
-    loadBloggerData():Q.Promise<string> {
+    loadBloggerData():Q.Promise<string> {        
         return Q.Promise<string>((resolve, reject) => {
             //resolve("this works!");
         });
     }
+    
 };

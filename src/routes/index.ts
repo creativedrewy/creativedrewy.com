4fb5619d640d6181f1935aa8571ff30127552558
Andrew Watson <awatson@gopro.com>
2016-05-24 17:20:49 -0500

    Add bio js route handling

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -8,17 +8,22 @@ const router = express.Router();
 var bloggerLoader:blogger.BloggerLoadService = new blogger.BloggerLoadService();
 
 /**
- * Handle site root route
+ * Handle site root
  */
 router.get('/', (req, res, next) => {
-  
   bloggerLoader.loadBloggerData()
     .then((posts) => {
       res.render('index', {
         bloggerPosts: posts
       });
     })
-    
 });
+
+/**
+ * "Dynamic" bio js that can be embedded elsewhere
+ */
+router.get('/bio.js', (req, res, next) => {
+  res.send("document.write('Principal Android and Interactive developer in Austin, TX. If it\\'s a cool project, I probably want to work on it.');");
+})
 
 export default router;

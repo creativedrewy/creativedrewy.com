5f10445a34f66c93e90aabc583cb4786183c362e
Andrew <creativedrewy@gmail.com>
2016-08-15 12:01:45 -0500

    Write rx processing and file downloading code; currently will not work! Next step is to get it to work.

--- src/service/GitHubLoadService.ts
+++ src/service/GitHubLoadService.ts
@@ -23,29 +23,56 @@ export class GitHubLoadService {
         });
     }
 
-    doSomeStuff() {
-        var gists = gitHubClient.gists.getAll({}, (err, res) => {
-            for (var gist of res) {
-                if (gist.description.startsWith("Quick")) {
-                    
+    loadQuickHitPosts():Observable<Array<PostDetails>> {
+        return this.generatePostsRxList()
+                .flatMap((listOfPosts) => {
+                    return Observable.from(listOfPosts);
+                })
+                .flatMap((post) => {
+                    return this.downloadUrl("host", "dirPath");
+                }, (origPost, fileContents) => {
+                    origPost.mainContent = fileContents;
+                    return origPost;
+                })
+                .toArray();
+    }
+
+    generatePostsRxList():Observable<Array<PostDetails>> {
+        return Observable.create<Array<PostDetails>>((subscriber) => {
+            gitHubClient.gists.getAll({}, (err, res) => {
+                var quickHitPosts = new Array<PostDetails>();
+
+                for (var gist of res) {
+                    if (gist.description.startsWith("Quick")) {
+                        var post = new PostDetails();
+                        post.title = "This is a test post";
+
+                        quickHitPosts.push(post);
+                    }
                 }
-            }
+
+                subscriber.onNext(quickHitPosts);
+            });
         });
     }
 
-    downloadUrl(host: string, dirPath: string) {
-        var urlRawData:string = "";
-            
-        https.get({ 
-            host: host,
-            path: dirPath
-        }, (response) => {
-            response.on('data', (chunk) => urlRawData += chunk)
-            
-            response.on('end', () => {
-                
-            });
+    downloadUrl(host: string, dirPath: string):Observable<string> {
+        return Observable.create<string>((subscriber) => { 
+            subscriber.onNext("Check me out I have some contents!")
         });
+
+        // var urlRawData:string = "";
+            
+        // https.get({ 
+        //     host: host,
+        //     path: dirPath
+        // }, (response) => {
+        //     response.on('data', (chunk) => urlRawData += chunk)
+            
+        //     response.on('end', () => {
+                
+        //     });
+        // });
     }
 
 }
